00ebeec89bf766eaa55ff57550bcf7b10cdf62d4
prashant Ranjan <persie@prashants-MacBook-Air.local>
2024-03-01 16:33:13 +0530

    ui fixes

--- src/Components/Servises/AiServises.jsx
+++ src/Components/Servises/AiServises.jsx
@@ -18,9 +18,9 @@ const AiServises = () => {
   };
   return (
     <>
-      <div className=" backdrop-blur-md bg-black z-50">
+      <div className=" ">
         <div className="flex  ">
-          <div className="w-full px-4  h-[484px] bg-black fixed inset-0   mt-20 backdrop-blur-sm flex z-10 justify-center ">
+          <div className="w-full px-4  h-[484px] bg-black fixed inset-0   mt-20  flex z-10 justify-center  ">
             <div className="flex flex-row gap-6">
               <div className="flex flex-col">
                 <div className="flex flex-row gap-6  mt-10">

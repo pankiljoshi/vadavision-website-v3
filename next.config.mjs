04d44555c64b95c2ec6652fe902f81f040f10985
Pankil Joshi <pankil@airbadge.us>
2024-06-23 17:32:36 +0530

    Upgraded packages and removed dev deps from deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          "vadavision.com", "blog.vadavision.com",
-        ],
-      },
 };
 
 export default nextConfig;
